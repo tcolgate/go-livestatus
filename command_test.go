d5646cab87caf5e0892003407a45502c95b6f661
Tristan Colgate <tristan@qubit.com>
2016-06-21 10:44:52 +0100

    Remove some old comments

--- command_test.go
+++ command_test.go
@@ -5,11 +5,6 @@ import (
 	"time"
 )
 
-//ACKNOWLEDGE_HOST_PROBLEM;<host_name>;<sticky>;<notify>;<persistent>;<author>;<comment>
-//ACKNOWLEDGE_SVC_PROBLEM;<host_name>;<service_description>;<sticky>;<notify>;<persistent>;<author>;<comment>
-//SCHEDULE_HOST_DOWNTIME;<host_name>;<start_time>;<end_time>;<fixed>;<trigger_id>;<duration>;<author>;<comment>
-//SCHEDULE_SVC_DOWNTIME;<host_name>;<service_desription><start_time>;<end_time>;<fixed>;<trigger_id>;<duration>;<author>;<comment>
-
 func Test_Cmd_AcknowledgeHostProblem(t *testing.T) {
 	expected := "COMMAND 12345678 ACKNOWLEDGE_HOST_PROBLEM;host;2;1;1;me@example.com;some stuff\n\n"
 
